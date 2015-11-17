e98e5f77514e77f2ef32d796121117f50b20669a
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-11-17 14:09:23 +0100

    Add debug statements

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
+import sys
+
 def main():
+    # The statement below is a debug print statement. The reason for it's existence (as it only contains "test") is unknown though.
+    # print("test")
     pass
