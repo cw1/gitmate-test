9e336875cf8bc51a73bbdcb9ee1b95199b4aecde
Lasse Schuirmann <lasse@schuirmann.net>
2015-11-21 15:02:07 +0100

    test: Add nonsense code

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
 def main():
     pass
+
+
+def a_lot_of_args(aaaa, bbbb, cccc, dddd, eeee, ffff, gggg, hhhh):
+    aaaa += bbbb
+    cccc += dddd
+    eeee += ffff
+    gggg += hhhh
+    
+    return aaaa
